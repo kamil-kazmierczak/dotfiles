832aa6576ddc806322663d7a075d8e34265d183a
kmilo <kamil.kazmierczak@pm.me>
2024-01-26 13:12:58 +0100

    Added hidden catalogs for fuzzy searching

--- .scripts/open-dir.sh
+++ .scripts/open-dir.sh
@@ -2,7 +2,7 @@
 
 cd ~
 
-DIR_PATH=$(fd --type directory --exclude .git --exclude .cache --exclude node_modules | fzf -m --height=20 --border=double --border-label=" Open directory " --border-label-pos=0:top --margin=1,5% --no-info --no-scrollbar --prompt='> ' --pointer='->' --marker='*' --tabstop=4)
+DIR_PATH=$(fd --type directory --hidden --exclude .git --exclude .cache --exclude node_modules | fzf -m --height=20 --border=double --border-label=" Open directory " --border-label-pos=0:top --margin=1,5% --no-info --no-scrollbar --prompt='> ' --pointer='->' --marker='*' --tabstop=4)
 
 if [[ -n "$DIR_PATH" ]]; then	
     cd $DIR_PATH || return
