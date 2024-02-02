b923614c62440ff69dc6c9104bfaa396018d2ab8
kamilo <kamil.kazmierczak@pm.me>
2024-02-02 17:56:59 +0100

    Small changes - basically simpler script

--- .scripts/open-file.sh
+++ .scripts/open-file.sh
@@ -2,8 +2,10 @@
 
 cd ~
 
-FILE_PATH=$(fd --type f --hidden --exclude .git --exclude node_modules --exclude .npm | fzf -m --height=20)
+FILE_PATH=$(fd --type f --hidden --exclude logseq --exclude .git --exclude node_modules --exclude .npm | fzf -m --height=10 --no-info)
 
+
+   
 if [[ -n "$FILE_PATH" ]]; then	
     nvim $FILE_PATH
 fi
